e515fb7fed5ed719ce013dcf9eeeb315b97f8613
Merszi <szabomerci77@gmail.com>
2023-11-08 14:04:58 +0100

    hamburger menu

--- reactwebsite/src/components/Navbar.js
+++ reactwebsite/src/components/Navbar.js
@@ -3,12 +3,20 @@ import "./Navbar.css";
 import { Menu } from "./Menu";
 
 class Navbar extends Component {
+    state = {clicked: false};
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
     render() {
         return(
             <nav className="NavbarItems">
                 <h1 className="navbar-logo">Dogschool</h1>
 
-                <ul className="nav-menu">
+                <div className="menu-icons" onClick= {this.handleClick}>
+                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                </div>
+
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
                     {Menu.map((item, index) => {
                        return(
                         <li key={index}>
