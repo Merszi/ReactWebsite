a7c5689f870d48a892199ba495224482d78af351
Merszi <szabomerci77@gmail.com>
2023-11-14 13:40:57 +0100

    background

--- reactwebsite/src/routes/HomePage.js
+++ reactwebsite/src/routes/HomePage.js
@@ -7,11 +7,11 @@ function Home() {
         <Navbar />
         <Hero 
             cName="hero"
-            heroImg="https://images.unsplash.com/photo-1587300003388-59208cc962cb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            //heroImg="https://images.unsplash.com/photo-1587300003388-59208cc962cb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             title="Welcome to Paws & Play Canine Hub!"
             text="At Paws & Play, we believe in creating a world where every dog is not just a pet but a cherished family member. Our canine hub is designed to cater to the diverse needs of your furry friends, offering a blend of education, fun, and comfort."
             btnText="Read more"
-            button="/"
+            button="/about"
             btnClass="show"
             />
        </> 
